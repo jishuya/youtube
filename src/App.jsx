491b75b847943c518be7e8b13256e0607d5d21bb
jishuya <jishuya3015@gmail.com>
2025-08-19 14:31:31 +0900

    ✨ [feat] Add searchHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import SearchHeader from './components/SearchHeader'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
 
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <Outlet />
+      <SearchHeader />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   )
 }
