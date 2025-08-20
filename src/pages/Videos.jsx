1aaa16872734a399d8a73feb1f42d38625ab1fad
jishuya <jishuya3015@gmail.com>
2025-08-20 15:30:13 +0900

    ✨ [feat] implement context provider

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
-import FakeYoutube from '../api/fakeYoutube';
-import Youtube from '../api/youtube';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 export default function Videos() {
     const { keyword } = useParams();
+    const { youtube } = useYoutubeApi();
     const {
         isLoading,
         error,
         data:videos, 
     } = useQuery({
             queryKey: ['videos', keyword ?? 'popular'],
-            queryFn: ()=> {
-                const youtube = new FakeYoutube();
-                // const youtube = new Youtube();
+            queryFn: () => {
                 return youtube.search(keyword);
             },
             staleTime: 60 * 1000,                      
